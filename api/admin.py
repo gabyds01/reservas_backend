51b9263a08541f46972b2f59b6ff1f9a9b095c02
gabyds01 <gabyds01@gmail.com>
2026-08-25 17:53:46 -0300

    feat: register models into an admin site

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Resource, Availability, Booking
+
+# Registramos los modelos para que sean visibles en el administrador
+#
+admin.site.register(User)
+admin.site.register(Resource)
+admin.site.register(Availability)
+admin.site.register(Booking)
