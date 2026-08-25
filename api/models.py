3bcf1f56774040d50dc8a4e1955d90268815e5bd
gabyds01 <gabyds01@gmail.com>
2026-08-25 13:13:03 -0300

    feat: add psycopg dependency and extend User model to allow use email for login.

--- api/models.py
+++ api/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import AbstractUser
+
 from django.db import models
 
 # Create your models here.
+
+class User(AbstractUser):
+    # Definimos los roles como opciones
+    CLIENT: str = 'client'
+    HOST: str = 'host'
+    ROLES_CHOICES: list[tuple[str, str]] = [
+        (CLIENT, 'Client'),
+        (HOST, 'Host'),
+    ]
+
+    # unique asegura que no haya duplicado
+    email = models.EmailField(unique=True)
+
+    # choices limita los roles a los definidos
+    rol = models.CharField(max_length=15, choices=ROLES_CHOICES, default=CLIENT)
+
+    # Por defecto django utiliza username para iniciar sesion
+    # podemos cambiar
+    USERNAME_FIELD = 'email'
+
+    # Internamente sigue necesitando username
+    REQUIRED_FIELDS = ['username']
--- config/settings.py
+++ config/settings.py
@@ -10,20 +10,24 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+_ = load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fv_ioox8$d9gi#pks0f+5@r9obs*is*+1x_88x*ni(rh*v-5v_'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG', False) == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +79,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASS'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
 
@@ -98,6 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'api.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.1/topics/i18n/
